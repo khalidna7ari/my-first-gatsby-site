b549caabf1cdbc3ae97c179e44b763ffe9e5b06a
Khalid Nahary <khalidn@Khalids-MacBook-Air.local>
2021-05-17 18:12:37 +0100

    add tailwind css and config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { footer } from "./footer.module.css";
-
 const Footer = () => {
   return (
     <footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { header } from "./header.module.css";
 
 const Header = () => {
   return (
-    <header className={header}>
+    <header>
       <h1>Header section</h1>
     </header>
   );
